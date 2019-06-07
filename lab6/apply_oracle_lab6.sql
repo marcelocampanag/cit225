4b604beaa9949b6f86db424e45b7dc076f51d898
Marcelo Campaña <cam17010@byui.edu>
2019-06-07 13:41:27 -0400

    exercise 1 to 3 ok

--- lab6/apply_oracle_lab6.sql
+++ lab6/apply_oracle_lab6.sql
@@ -27,9 +27,33 @@ SPOOL apply_oracle_lab6.txt
 -- ... insert lab 6 commands here ...
 --1
 
-ALTER TABLE RENTAL_ITEM ADD RENTAL_ITEM_PRICE NUMBER(22),
-ALTER TABLE RENTAL_ITEM ADD RENTAL_ITEM_TYPE NUMBER(22),
+ALTER TABLE RENTAL_ITEM ADD RENTAL_ITEM_PRICE NUMBER(22);
+ALTER TABLE RENTAL_ITEM ADD RENTAL_ITEM_TYPE NUMBER(22);
 ALTER TABLE RENTAL_ITEM ADD FOREIGN KEY(RENTAL_ITEM_TYPE) REFERENCES COMMON_LOOKUP(COMMON_LOOKUP_ID);
+
+-- select table query
+
+SET NULL ''
+COLUMN table_name   FORMAT A14
+COLUMN column_id    FORMAT 9999
+COLUMN column_name  FORMAT A22
+COLUMN data_type    FORMAT A12
+SELECT   table_name
+,        column_id
+,        column_name
+,        CASE
+           WHEN nullable = 'N' THEN 'NOT NULL'
+           ELSE ''
+         END AS nullable
+,        CASE
+           WHEN data_type IN ('CHAR','VARCHAR2','NUMBER') THEN
+             data_type||'('||data_length||')'
+           ELSE
+             data_type
+         END AS data_type
+FROM     user_tab_columns
+WHERE    table_name = 'RENTAL_ITEM'
+ORDER BY 2;
 
 --2
 
@@ -53,12 +77,49 @@ FOREIGN KEY(CREATED_BY) REFERENCES SYSTEM_USER(SYSTEM_USER_ID),
 CONSTRAINT YN_PRICE CHECK( active_flag IN ('Y','N'))
 );
 
----3
+-- select table query
+
+SET NULL ''
+COLUMN table_name   FORMAT A14
+COLUMN column_id    FORMAT 9999
+COLUMN column_name  FORMAT A22
+COLUMN data_type    FORMAT A12
+SELECT   table_name
+,        column_id
+,        column_name
+,        CASE
+           WHEN nullable = 'N' THEN 'NOT NULL'
+           ELSE ''
+         END AS nullable
+,        CASE
+           WHEN data_type IN ('CHAR','VARCHAR2','NUMBER') THEN
+             data_type||'('||data_length||')'
+           ELSE
+             data_type
+         END AS data_type
+FROM     user_tab_columns
+WHERE    table_name = 'PRICE'
+ORDER BY 2;
+
+-- select constraint query
+
+COLUMN constraint_name   FORMAT A16
+COLUMN search_condition  FORMAT A30
+SELECT   uc.constraint_name
+,        uc.search_condition
+FROM     user_constraints uc INNER JOIN user_cons_columns ucc
+ON       uc.table_name = ucc.table_name
+AND      uc.constraint_name = ucc.constraint_name
+WHERE    uc.table_name = UPPER('price')
+AND      ucc.column_name = UPPER('active_flag')
+AND      uc.constraint_name = UPPER('yn_price')
+AND      uc.constraint_type = 'C';
+
+--- 3
 
 
-UPDATE   common_lookup
-SET      common_lookup_table = 'TELEPHONE'
-,        common_lookup_column = '	TELEPHONE_TYPE'
-WHERE    common_lookup_context = 'MULIPLE';
+
+
 --5
-SPOOL OFF
+
+
