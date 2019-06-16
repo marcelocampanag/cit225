ccda4dde553e7bf5e40f4011b4331a8de5c0564a
Marcelo Campaña <cam17010@byui.edu>
2019-06-15 23:28:56 -0400

    insert item rental ok. review commetaries

--- lab6/apply_oracle_lab6.sql
+++ lab6/apply_oracle_lab6.sql
@@ -612,8 +612,8 @@ SPOOL apply_oracle_lab6.txt
 	  WHERE    system_user_name = 'SYSADMIN')         -- last_updated_by
 	, SYSDATE                                         -- last_update_date
 	);
-	
-	
+
+
 COLUMN account_number  FORMAT A10  HEADING "Account|Number"
 COLUMN full_name       FORMAT A16  HEADING "Name|(Last, First MI)"
 COLUMN street_address  FORMAT A14  HEADING "Street Address"
@@ -636,6 +636,184 @@ ON       c.contact_id = a.contact_id INNER JOIN street_address sa
 ON       a.address_id = sa.address_id INNER JOIN telephone t
 ON       c.contact_id = t.contact_id
 WHERE    c.last_name = 'Potter';
+
+-- EXERCISE 3.d
+
+INSERT INTO rental
+( rental_id
+, customer_id
+, check_out_date
+, return_date
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date )
+VALUES
+( rental_s1.nextval
+,(SELECT   contact_id
+  FROM     contact
+  WHERE    last_name = 'Potter'
+  AND      first_name = 'Harry')
+, TRUNC(SYSDATE)
+, TRUNC(SYSDATE) + 1
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental
+( rental_id
+, customer_id
+, check_out_date
+, return_date
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date )
+VALUES
+( rental_s1.nextval
+,(SELECT   contact_id
+  FROM     contact
+  WHERE    last_name = 'Potter'
+  AND      first_name = 'Ginny')
+, TRUNC(SYSDATE)
+, TRUNC(SYSDATE) + 3
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental
+( rental_id
+, customer_id
+, check_out_date
+, return_date
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date )
+VALUES
+( rental_s1.nextval
+,(SELECT   contact_id
+  FROM     contact
+  WHERE    last_name = 'Potter'
+  AND      first_name = 'Lily')
+, TRUNC(SYSDATE)
+, TRUNC(SYSDATE) + 5
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental_item
+( rental_item_id
+, rental_id
+, item_id
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date)
+VALUES
+( rental_item_s1.nextval
+,(SELECT   r.rental_id
+  FROM     rental r
+  ,        contact c
+  WHERE    r.customer_id = c.contact_id
+  AND      c.last_name = 'Vizquel'
+  AND      c.first_name = 'Oscar')
+,(SELECT   i.item_id
+  FROM     item i
+  ,        common_lookup cl
+  WHERE    i.item_title = 'Star Wars I'
+  AND      i.item_subtitle = 'Phantom Menace'
+  AND      i.item_type = cl.common_lookup_id
+  AND      cl.common_lookup_type = 'DVD_WIDE_SCREEN')
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental_item
+( rental_item_id
+, rental_id
+, item_id
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date)
+VALUES
+( rental_item_s1.nextval
+,(SELECT   r.rental_id
+  FROM     rental r inner join contact c
+  ON       r.customer_id = c.contact_id
+  WHERE    c.last_name = 'Vizquel'
+  AND      c.first_name = 'Oscar')
+,(SELECT   d.item_id
+  FROM     item d join common_lookup cl
+  ON       d.item_title = 'Star Wars II'
+  WHERE    d.item_subtitle = 'Attack of the Clones'
+  AND      d.item_type = cl.common_lookup_id
+  AND      cl.common_lookup_type = 'DVD_WIDE_SCREEN')
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental_item
+( rental_item_id
+, rental_id
+, item_id
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date)
+VALUES
+( rental_item_s1.nextval
+,(SELECT   r.rental_id
+  FROM     rental r
+  ,        contact c
+  WHERE    r.customer_id = c.contact_id
+  AND      c.last_name = 'Vizquel'
+  AND      c.first_name = 'Oscar')
+,(SELECT   d.item_id
+  FROM     item d
+  ,        common_lookup cl
+  WHERE    d.item_title = 'Star Wars III'
+  AND      d.item_subtitle = 'Revenge of the Sith'
+  AND      d.item_type = cl.common_lookup_id
+  AND      cl.common_lookup_type = 'DVD_WIDE_SCREEN')
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
+
+INSERT INTO rental_item
+( rental_item_id
+, rental_id
+, item_id
+, created_by
+, creation_date
+, last_updated_by
+, last_update_date)
+VALUES
+( rental_item_s1.nextval
+,(SELECT   r.rental_id
+  FROM     rental r
+  ,        contact c
+  WHERE    r.customer_id = c.contact_id
+  AND      c.last_name = 'Vizquel'
+  AND      c.first_name = 'Doreen')
+,(SELECT   d.item_id
+  FROM     item d
+  ,        common_lookup cl
+  WHERE    d.item_title = 'I Remember Mama'
+  AND      d.item_subtitle IS NULL
+  AND      d.item_type = cl.common_lookup_id
+  AND      cl.common_lookup_type = 'BLU-RAY')
+, 1001
+, SYSDATE
+, 1001
+, SYSDATE);
 
 -- 4
 SPOOL OFF
